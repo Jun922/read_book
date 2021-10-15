2ef27199a8ddefe93a4cf66d0ddfd746fc162a4d
Jun922 <junjunchi620@gmail.com>
2021-10-15 23:46:56 +0900

    test

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+  
+  describe 'ユーザー新規登録' do
+    #正常系
+    context '新規登録できる時' do
+      it '全ての項目の入力が存在すれば登録できること' do
+        expect(@user).to be_valid
+      end
+
+      it 'emailが＠つきであれば登録できる' do
+        @user.email = 'test@test'
+        expect(@user).to be_valid
+      end
+
+      it 'passwordとpassword_confirmationが6文字以上半角英数字であれば登録できる' do
+        @user.password = 'aaa000'
+        @user.password_confirmation = 'aaa000'
+        expect(@user).to be_valid
+      end
+    end
+
+    #異常系
+    context '新規登録できない時' do
+      it 'emailが空では登録できない' do
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Email can't be blank"
+      end
+
+      it 'メールアドレスに@がない場合登録できない' do
+        @user.email = 'test'
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Email is invalid"
+      end
+
+      it 'passwordが空では登録できない' do
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Password can't be blank"
+      end
+        
+      it 'password_confirmationが空では登録できない' do
+        @user.password_confirmation = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Password confirmation doesn't match Password"
+      end
+    end
+  end
+end
